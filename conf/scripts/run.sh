befb7dbdb9bcd2af3f1a03943b18427c4376f765
Alexander Nesterov <alex19pov31@gmail.com>
2020-12-27 00:58:16 +0300

    update run script

--- conf/scripts/run.sh
+++ conf/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql" ]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql
 else
     chown -R mysql:mysql /var/lib/mysql
